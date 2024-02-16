8254d304a1cce9bb1b225223fd32064538f7b1f4
Doubtkumar <micromax.rohit.rs@gmail.com>
2024-02-16 16:20:12 +0530

    beluga project

---
+++ src/main/java/com/beluga/belugaproject/BelugaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.beluga.belugaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelugaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelugaProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/beluga/belugaproject/controller/BelugaController.java
@@ -0,0 +1,40 @@
+package com.beluga.belugaproject.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beluga.belugaproject.model.Consignment;
+import com.beluga.belugaproject.repo.ConsignmentRepo;
+
+@RestController
+@RequestMapping("beluga/v1")
+public class BelugaController {
+	
+	@Autowired
+	private ConsignmentRepo consignmentRepo;
+
+	@GetMapping("/getAllConsignment")
+	public List<Consignment>  start() {
+		return consignmentRepo.findAll();
+	}
+	
+	@PostMapping("/createConsignment")
+	public ResponseEntity<Consignment> createConsignment(@RequestBody Consignment consignment) {
+		return new ResponseEntity<Consignment>(consignmentRepo.save(consignment),HttpStatus.CREATED);
+	}
+		
+	@SuppressWarnings("deprecation")
+	@PostMapping("/getConsignment/{id}")
+	public ResponseEntity<Consignment> getConsignment(@PathVariable long id){
+		return new ResponseEntity<Consignment>(consignmentRepo.findById(id).get(),HttpStatus.FOUND);
+	}
+}
